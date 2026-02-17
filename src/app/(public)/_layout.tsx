599675e8af09d7d87a7ec641d78484e9b3218dbe
Cédric Fressin <hello@cedricfressin.com>
2026-02-17 09:44:46 +0100

    fix(config): remove unsupported React Server experiments breaking web build

--- src/app/(public)/_layout.tsx
+++ src/app/(public)/_layout.tsx
@@ -4,6 +4,11 @@ import { View } from 'react-native'
 import { PublicFooter } from '~/features/navigation/public-footer'
 import { PublicHeader } from '~/features/navigation/public-header'
 
+export const unstable_settings = {
+  // Ensure any route can link back to `/`
+  anchor: 'index'
+}
+
 export default function PublicLayout() {
   return (
     <View>
