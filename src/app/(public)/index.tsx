670b8397727d009bc6f9a34be76023a3fae27aba
Cédric Fressin <hello@cedricfressin.com>
2026-02-17 09:41:38 +0100

    chore(config): enable React Server Functions and static page generation

--- src/app/(public)/index.tsx
+++ src/app/(public)/index.tsx
@@ -2,6 +2,11 @@ import Head from 'expo-router/head'
 import { View } from 'react-native'
 import { Text } from '~/components/ui/text'
 
+export const unstable_settings = {
+  // This component will be rendered at build-time and never re-rendered in production.
+  render: 'static'
+}
+
 export default function LandingPage() {
   return (
     <>
